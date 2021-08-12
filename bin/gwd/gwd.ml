45e07d3f890e6281be2d49f97c6a69007443515c
A2 <a2line@gmail.com>
2021-08-12 17:02:38 +0200

    add .map support in gwd

--- bin/gwd/gwd.ml
+++ bin/gwd/gwd.ml
@@ -1466,6 +1466,7 @@ type misc_fname =
   | Css of string
   | Eot of string
   | Js of string
+  | Map of string
   | Otf of string
   | Other of string
   | Png of string
@@ -1478,9 +1479,10 @@ let content_misc conf len misc_fname =
   Output.status conf Def.OK;
   let (fname, t) =
     match misc_fname with
-    | Css fname -> fname, "text/css"
+    | Css fname -> fname, "text/css; charset=UTF-8"
     | Eot fname -> fname, "application/font-eot"
-    | Js fname -> fname, "text/javascript"
+    | Js fname -> fname, "text/javascript; charset=UTF-8"
+    | Map fname -> fname, "application/json"
     | Otf fname -> fname, "application/font-otf"
     | Other fname -> fname, "text/plain"
     | Png fname -> fname, "image/png"
@@ -1515,6 +1517,7 @@ let print_misc_file conf misc_fname =
   | Css fname
   | Eot fname
   | Js fname
+  | Map fname
   | Otf fname
   | Png fname
   | Svg fname
@@ -1544,6 +1547,7 @@ let misc_request conf fname =
     let misc_fname =
       if Filename.check_suffix fname ".css" then Css fname
       else if Filename.check_suffix fname ".js" then Js fname
+      else if Filename.check_suffix fname ".map" then Map fname
       else if Filename.check_suffix fname ".otf" then Otf fname
       else if Filename.check_suffix fname ".svg" then Svg fname
       else if Filename.check_suffix fname ".woff" then Woff fname
