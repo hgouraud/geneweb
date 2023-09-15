60d30892fbe79ac19ddbe71d86d965ab6751d4ae
Henri83 <henri.gouraud@laposte.net>
2023-09-15 12:16:39 +0200

    correction for d[e |']; cleanup titles in dag.txt; some experiments with ::|xxx|

--- lib/templ.ml
+++ lib/templ.ml
@@ -647,28 +647,21 @@ and eval_transl_lexicon conf upp s c =
           if String.length s2 > 0 && s2.[0] = '|' then
             let i = 1 in
             let j = String.rindex s2 '|' in
-            if j = 0 then
-              let s2 = String.sub s2 i (String.length s2 - j - 1) in
-              try apply_format conf nth s1 s2
-              with Failure _ -> raise Not_found
-            else
-              let s3 =
-                let s = String.sub s2 i (j - i) in
-                let s = s ^ c in
-                let astl =
-                  Templ_parser.parse_templ conf (Lexing.from_string s)
-                in
-                List.fold_left (fun s a -> s ^ eval_ast conf a) "" astl
-              in
-              let s4 = String.sub s2 (j + 1) (String.length s2 - j - 1) in
-              let _s5 =
-                match nth with
-                | Some n -> Util.transl_nth conf s4 n
-                | None -> Util.transl conf s4
-              in
-              let s2 = s3 ^ s4 in
-              try apply_format conf nth s1 s2
-              with Failure _ -> raise Not_found
+            let _k = skip_spaces_and_newlines s2 (j + 1) in
+            let s3 =
+              let s = String.sub s2 i (j - i) in
+              let s = s ^ c in
+              let astl = Templ_parser.parse_templ conf (Lexing.from_string s) in
+              List.fold_left (fun s a -> s ^ eval_ast conf a) "" astl
+            in
+            let s4 = String.sub s2 (j + 1) (String.length s2 - j - 1) in
+            let _s5 =
+              match nth with
+              | Some n -> Util.transl_nth conf s4 n
+              | None -> Util.transl conf s4
+            in
+            let s2 = s3 ^ s4 in
+            try apply_format conf nth s1 s2 with Failure _ -> raise Not_found
           else if String.length s2 > 0 && s2.[0] = ':' then
             (* this is a third colon *)
             let s2 = String.sub s2 1 (String.length s2 - 1) in
